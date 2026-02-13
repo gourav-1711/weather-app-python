40f87979c5272c606ebed6b04b29435347048f26
gourav-1711 <gouravdadhich34@gmail.com>
2026-02-13 19:47:34 +0530

    added kivy md

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+```
+main.py
+.env
+app/
+    ├── 📁 components
+    ├── 📁 lib
+    │   └── 🐍 api.py
+    ├── 📄 weather_app.kv
+    └── 🐍 app.py
+```
+
--- main.py
+++ main.py
@@ -1,12 +1,32 @@
-from kivy.app import App
-from kivy.uix.label import Label
+from kivymd.app import MDApp
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.label import MDLabel
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.button import MDButton, MDButtonIcon, MDButtonText
+class Layout(MDBoxLayout):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        label = MDLabel(
+                text="Hello World",
+                halign="center",
+                pos_hint={"center_x": 0.5, "center_y": 0.5},
+                text_color="red"
+            )
+        self.add_widget(label)
+        button = MDButton(MDButtonText(
+                        text="Elevated",
+                    ))
+      
+        self.add_widget(button)
 
-def main():
-    pass
-
-app = App()
-app.run()
-
+class WeatherApp(MDApp):
+    def build(self):
+        screen = MDScreen(md_bg_color="white")
+        
+        layout = Layout()
+        screen.add_widget(layout)
+        
+        return screen
 
 if __name__ == "__main__":
-    main()
+    WeatherApp().run()
